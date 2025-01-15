5a364ab0f611cdc1fbf3f6e67d3d0abd0b1597f9
hosseinmirzapur <hosseinmirzapur@gmail.com>
2025-01-15 12:25:47 +0330

    feat(models): departments

--- app/models/department.ts
+++ app/models/department.ts
@@ -1,5 +1,8 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import Folder from '#models/folder'
+import DepartmentUser from '#models/department_user'
+import type { HasMany } from '@adonisjs/lucid/types/relations'
 
 export default class Department extends BaseModel {
   @column({ isPrimary: true })
@@ -10,4 +13,10 @@ export default class Department extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
+
+  @hasMany(() => Folder)
+  declare folders: HasMany<typeof Folder>
+
+  @hasMany(() => DepartmentUser)
+  declare departmentUsers: HasMany<typeof DepartmentUser>
 }
