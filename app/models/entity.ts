e7442f486b051b5f2a7f66856d84421fbac3396a
hosseinmirzapur <hosseinmirzapur@gmail.com>
2025-01-15 16:25:00 +0330

    feat(models): entity

--- app/models/entity.ts
+++ app/models/entity.ts
@@ -1,5 +1,10 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import EntityGroup from '#models/entity_group'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import drive from '@adonisjs/drive/services/main'
+import { DriveDisks } from '@adonisjs/drive/types'
+import { Exception } from '@adonisjs/core/exceptions'
 
 export default class Entity extends BaseModel {
   @column({ isPrimary: true })
@@ -10,4 +15,45 @@ export default class Entity extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
+
+  @column()
+  declare meta: any
+
+  @column()
+  declare result_location: any
+
+  @column()
+  declare type: keyof DriveDisks
+
+  @column()
+  declare file_location: string
+
+  @belongsTo(() => EntityGroup)
+  declare entityGroup: BelongsTo<typeof EntityGroup>
+
+  public async getFileData(): Promise<string | null> {
+    const data = await drive.use(this.type).get(this.file_location)
+
+    if (!data) {
+      throw new Exception(`Failed to get file data for entity #${this.id}`)
+    }
+
+    return data
+  }
+
+  public async getExtensionFile(): Promise<string> {
+    const fileLocation = this.file_location
+    return fileLocation.split('.').pop() as string
+  }
+
+  public async fileNotExists(): Promise<boolean> {
+    return !this.fileExists()
+  }
+
+  public async fileExists(): Promise<boolean> {
+    if (!this.file_location) {
+      return false
+    }
+    return drive.use(this.type).exists(this.file_location)
+  }
 }
