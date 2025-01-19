dacb051b75a574bebd5d1c47b0fa43985ba1d996
hosseinmirzapur <hosseinmirzapur@gmail.com>
2025-01-19 20:32:26 +0330

    feat(services): AI

--- app/services/ai_service.ts
+++ app/services/ai_service.ts
@@ -1,3 +1,120 @@
+import { Exception } from '@adonisjs/core/exceptions'
+import config from '@adonisjs/core/services/config'
+import logger from '@adonisjs/core/services/logger'
+import axios from 'axios'
+import fs from 'node:fs'
+
 export class AiService {
-  // Your code here
+  public async submitToOcr(filePath: string, isImage: boolean = false): Promise<any> {
+    let url = config.get('ai.ocr.url') as string
+
+    if (isImage) {
+      url = `${url}original`
+    }
+
+    try {
+      const form = new FormData()
+      const file = fs.createReadStream(filePath)
+      form.append('file', file)
+      const response = await axios.post(url, form, {
+        headers: {
+          Accept: 'application/json',
+        },
+      })
+
+      if (response.status !== 200) {
+        throw new Exception(
+          `An error occurred in submitting image to OCR status: ${response.status}`
+        )
+      }
+
+      return response.data
+    } catch (error) {
+      throw new Exception(
+        `An error occurred in submitting image to OCR status: ${error.response?.status || 500}`
+      )
+    }
+  }
+
+  public async downloadSearchableFile(link: string): Promise<any> {
+    const url = `${config.get('ai.ocr.downloadSearchablePdfLink') as string}${link}`
+
+    try {
+      const response = await axios.get(url)
+
+      if (response.status !== 200) {
+        throw new Exception(
+          `An error occurred in downloading searchable file from OCR status: ${response.status}`
+        )
+      }
+
+      return response.data
+    } catch (error) {
+      throw new Exception(
+        `An error occurred in downloading searchable file from OCR status: ${
+          error.response?.status || 500
+        }`
+      )
+    }
+  }
+
+  public async submitToAsr(filepath: string): Promise<any> {
+    const url = config.get('ai.stt.url') as string
+
+    try {
+      const form = new FormData()
+      form.append('name', 'audio_file')
+      const contents = fs.createReadStream(filepath)
+      form.append('contents', contents)
+      const response = await axios.post(url, form, {
+        headers: {
+          Accept: 'application/json',
+        },
+      })
+
+      if (response.status !== 200) {
+        throw new Exception(
+          `An error occurred in submitting audio to ASR status: ${response.status}`
+        )
+      }
+
+      logger.info(response.data)
+
+      return response.data
+    } catch (error) {
+      throw new Exception(
+        `An error occurred in submitting audio to ASR status: ${error.response?.status || 500}`
+      )
+    }
+  }
+
+  public async getVoiceWindows(content: string): Promise<any> {
+    const url = config.get('ai.stt.voice_splitter_url') as string
+
+    try {
+      const form = new FormData()
+      form.append('name', 'file')
+      form.append('contents', content)
+      form.append('filename', 'name.wav')
+
+      const res = await axios.post(url, form, {
+        timeout: 900,
+      })
+
+      if (res.status !== 200) {
+        throw new Exception(`An error occurred in submitting audio to ASR status: ${res.status}`)
+      }
+
+      const data = res.data
+      if (!data?.prediction) {
+        throw new Exception(`Failed to get prediction. Response ${res.data}`)
+      }
+
+      return data.prediction
+    } catch (error) {
+      throw new Exception(
+        `An error occurred in submitting audio to ASR status: ${error.response?.status || 500}`
+      )
+    }
+  }
 }
---
+++ config/ai.ts
@@ -0,0 +1,10 @@
+export default {
+  ocr: {
+    url: 'http://45.82.139.60:8221/convert_searchable/',
+    downloadSearchablePdfLink: 'http://45.82.139.60:8221/download/',
+  },
+  stt: {
+    url: 'http://45.82.139.60:6969/gpu/transcribe/',
+    voice_splitter_url: 'http://45.82.139.60:8222/',
+  },
+}
