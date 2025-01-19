5913c71272cc8b245910b95f4a42a76a478942bf
hosseinmirzapur <hosseinmirzapur@gmail.com>
2025-01-19 13:51:07 +0330

    feat(models): notification

--- app/models/notification.ts
+++ app/models/notification.ts
@@ -1,7 +1,14 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import User from '#models/user'
+import Letter from '#models/letter'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
 
 export default class Notification extends BaseModel {
+  static PRIORITY_NORMAL = 'NORMAL'
+  static PRIORITY_IMMEDIATELY = 'IMMEDIATELY'
+  static PRIORITY_INSTANT = 'INSTANT'
+
   @column({ isPrimary: true })
   declare id: number
 
@@ -11,7 +18,28 @@ export default class Notification extends BaseModel {
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
 
-  static PRIORITY_NORMAL = 'NORMAL'
-  static PRIORITY_IMMEDIATELY = 'IMMEDIATELY'
-  static PRIORITY_INSTANT = 'INSTANT'
+  @column()
+  declare meta: Record<string, any>
+
+  @column()
+  declare priority: string
+
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
+
+  @belongsTo(() => Letter)
+  declare letter: BelongsTo<typeof Letter>
+
+  public getPriorityNotification(): string {
+    switch (this.priority) {
+      case Notification.PRIORITY_NORMAL:
+        return 'عادی'
+      case Notification.PRIORITY_IMMEDIATELY:
+        return 'فوری'
+      case Notification.PRIORITY_INSTANT:
+        return 'آنی'
+      default:
+        throw new Error('unsupported priority notification')
+    }
+  }
 }
