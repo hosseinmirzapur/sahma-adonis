13adc1310b72e4413ccfcfcd852d188a2e5b30a5
hosseinmirzapur <hosseinmirzapur@gmail.com>
2025-01-19 13:04:09 +0330

    feat(models): letter sign

--- app/models/letter_sign.ts
+++ app/models/letter_sign.ts
@@ -1,5 +1,8 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import Letter from '#models/letter'
+import User from '#models/user'
 
 export default class LetterSign extends BaseModel {
   @column({ isPrimary: true })
@@ -10,4 +13,10 @@ export default class LetterSign extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
+
+  @belongsTo(() => Letter)
+  declare letter: BelongsTo<typeof Letter>
+
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
 }
